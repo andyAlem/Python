e311e9a5382d9fe214a155a9baf79109ecf76358
Andrej <andrej@mail.com>
2024-11-19 18:12:13 +0100

    add if_main

--- scr/widget.py
+++ scr/widget.py
@@ -2,7 +2,7 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(cards_accounts_data: str) -> str:
-    """ Функция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
+    """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
     if cards_accounts_data.startswith("Счет" or "Счёт"):
         account_number = cards_accounts_data.split(maxsplit=1)[1]
         return f"Счет {get_mask_account(account_number)}"
@@ -11,11 +11,11 @@ def mask_account_card(cards_accounts_data: str) -> str:
         return f"{card_type} {get_mask_card_number(card_number)}"
 
 
-
 def get_date(date: str) -> str:
     """Функция, которая изменяет дату в нужном формате"""
 
     return f"{(date[8:10])}.{(date[5:7])}.{(date[0:4])}"
 
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(get_date("2024-03-11T02:26:18.671407"))
+if __name__ == "__main__":
+    print(mask_account_card("Счет Platinum 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
