91e75b172bd64d590900f0dd8230fad3995fd401
Andrej <andrej@mail.com>
2024-11-25 21:40:49 +0100

    added function filter_by_state

---
+++ scr/processing.py
@@ -0,0 +1,14 @@
+
+dictionaries_to_filter = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+def filter_by_state(dictionaries_to_filter: list, state: str ='EXECUTED') -> list :
+    """Функция возвращает список словарей по фильтру значения state"""
+
+    return [i for i in dictionaries_to_filter if i.get('state') == state]
+
+print(filter_by_state(dictionaries_to_filter))
