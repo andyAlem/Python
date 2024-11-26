457f8798c72ea0b2538825f27001fdd3ab6cf52b
Andrej <andrej@mail.com>
2024-11-26 23:23:46 +0100

    used isort for imports

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
-from tests.conftest import card_number_empty, card_nonstandard_cards
+from tests.conftest import card_nonstandard_cards, card_number_empty
 
 
 @pytest.mark.parametrize("card_number, expected",
