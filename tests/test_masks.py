d64bd6bcb5bc6b2b2d078ba8572f54c0c3d008bc
Andrej <andrej@mail.com>
2024-11-26 22:51:52 +0100

    test if card number get masked

---
+++ tests/test_masks.py
@@ -0,0 +1,18 @@
+import pytest
+from src.masks import get_mask_account, get_mask_card_number
+
+@pytest.mark.parametrize("card_number, expected",
+    [
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("6831982476737658", "6831 98** **** 7658"),
+        ("8990922113665229", "8990 92** **** 5229"),
+        ("5999414228426353", "5999 41** **** 6353")
+    ],
+)
+
+def test_get_mask_card_number(card_number, expected):
+    """Тестирование правильности маскирования номера карты"""
+    assert get_mask_card_number(card_number) == expected
+
+
