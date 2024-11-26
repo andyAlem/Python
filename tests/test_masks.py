3c7085f39950c009c38e8518049a22b8f2558c97
Andrej <andrej@mail.com>
2024-11-26 23:11:52 +0100

    test for empy card number

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,7 @@
 import pytest
 from src.masks import get_mask_account, get_mask_card_number
+from tests.conftest import card_number_empty
+
 
 @pytest.mark.parametrize("card_number, expected",
     [
@@ -7,7 +9,6 @@ from src.masks import get_mask_account, get_mask_card_number
         ("7158300734726758", "7158 30** **** 6758"),
         ("6831982476737658", "6831 98** **** 7658"),
         ("8990922113665229", "8990 92** **** 5229"),
-        ("5999414228426353", "5999 41** **** 6353")
     ],
 )
 
@@ -15,4 +16,6 @@ def test_get_mask_card_number(card_number, expected):
     """Тестирование правильности маскирования номера карты"""
     assert get_mask_card_number(card_number) == expected
 
-
+def test_get_mask_card_number_empty(card_number_empty):
+    """Проверка, что функция корректно обрабатывает входные строки, где отсутствует номер карты."""
+    assert get_mask_card_number(card_number_empty)
