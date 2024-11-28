4cd649299361f67d44b249c8235e3bb50de15f4c
Andrej <andrej@mail.com>
2024-11-28 23:11:09 +0100

    modul generater is finished

--- src/generators.py
+++ src/generators.py
@@ -1,106 +1,27 @@
 def filter_by_currency(transactions, currency):
     """Функция принимает на вход список словарей, представляющих транзакции.
     Функция должна возвращает итератор, который поочередно выдает транзакции,
-    где валюта операции соответствует заданной (например, USD). """
-# https://sky.pro/media/ispolzovanie-dict-getkey-vmesto-dictkey-v-python/
+    где валюта операции соответствует заданной (например, USD)."""
+    # https://sky.pro/media/ispolzovanie-dict-getkey-vmesto-dictkey-v-python/
     for transaction in transactions:
-        if transaction.get('operationAmount', {}).get('currency', {}).get('code') == currency:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
-
-
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
-
-
 
 
 def transaction_descriptions(transactions):
     """Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
 
     for transaction in transactions:
-        description = transaction.get('description', 'Описание транзакции отсутствует ')
+        description = transaction.get("description", "Описание транзакции отсутствует ")
         yield description
 
-descriptions = transaction_descriptions(transactions)
 
-for _ in range(2):
-    print(next(descriptions))
+def card_number_generator(start, stop):
+    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты."""
 
+    if not (1 <= start <= stop <= 9999_9999_9999_9999):
+        raise ValueError("Некорректный диапазон")
 
+    for number in range(start, stop + 1):
+        card_number = f"{number:016d}"
+        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
