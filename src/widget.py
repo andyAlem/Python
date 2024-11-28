000cfd61f29956af23a6d4bb09d4602792e1e61d
Andrej <andrej@mail.com>
2024-11-28 07:47:32 +0100

    new tests for function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(cards_accounts_data: str) -> str:
     """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
-    if cards_accounts_data.startswith("Счет" or "Счёт"):
+    if cards_accounts_data.startswith(("Счет", "Счёт")):
         account_number = cards_accounts_data.split(maxsplit=1)[1]
         return f"Счет {get_mask_account(account_number)}"
     else:
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,53 +3,32 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-def test_mask_account_card_input_account(inputs_mask_account):
-    """ "Тест для проверки маскировки аккаунта"""
-    assert mask_account_card(inputs_mask_account)
+@pytest.mark.parametrize("input_card_or_account,expected_mask", [
+    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+    ("Счет 73654108430135874305", "Счет **4305"),
+    ("Счет 64686473678894779589", "Счет **9589"),
+    ("Счёт 35383033474447895560", "Счет **5560"),
+])
+def test_mask_account_card(input_card_or_account, expected_mask):
+    """  Тестирования функции для разных типов карт и номера счета.
+    Учитывает разные форматы написания "Cчет или Счёт" """
+    assert mask_account_card(input_card_or_account) == expected_mask
+
+def test_mask_account_wrong_length():
+    """Проверка, что функция корректно обрабатывает входные данные,
+        где номер счета меньше ожидаемой длины."""
+    assert mask_account_card("Счёт 007") == "Счет **007"
 
 
-def test_mask_account_card_input_card(inputs_mask_account):
-    """Тест для проверки маскировки банковской карты"""
-    assert mask_account_card(inputs_mask_account)
-
-
-@pytest.mark.parametrize(
-    "input_card_or_account_number, mask",
-    [
-        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-        ("Счет 35383033474447895560", "Счет **5560"),
-    ],
-)
-def test_mask_account_card(input_card_or_account_number, mask):
-    """Параметризованные тесты с разными типами карт и счетов для проверки универсальности функции."""
-    assert mask_account_card(input_card_or_account_number) == mask
-
-
-@pytest.mark.parametrize("input_non_standard", [(""), ("asiV"), ("Шито"), ("11112")])
-def test_mask_account_card_invalid_input(input_non_standard):
-    """Тестирование функции на обработку некорректных входных данных
-    и проверка ее устойчивости к ошибкам."""
-    with pytest.raises(ValueError):
-        mask_account_card(input_non_standard)
-
-
-def test_get_date():
-    """Тест на правильность преобразования даты."""
+def test_get_date_correct_format():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
 
-@pytest.mark.parametrize(
-    "date_format_not_correct",
-    [
-        "",
-        "for test",
-        "02/58/8888",
-        "1989-1-3",
-    ],
-)
-def test_get_date_missing_date(date_format_not_correct):
-    """Проверка работы функции на различных входных форматах даты, нестандартные строки с датами.
-    Проверка, что функция корректно обрабатывает входные строки, где отсутствует дата."""
-    assert get_date(date_format_not_correct)
+
+
+
+
+
