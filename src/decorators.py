94a7d6265586cd52ecd5599472524e82b36633d8
Andrej <andrej@mail.com>
2024-12-03 22:20:02 +0100

    decorator is done. Start and Stop Time has to be added

---
+++ src/decorators.py
@@ -0,0 +1,23 @@
+from functools import wraps
+
+def log(filename:None)
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                log_message = 'my_function ok'
+                print(log_message)
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(log_message + "\n")
+                return result
+            except Exception as ex:
+                log_message = f"{func.__name__} Ошибка : {ex}. Inputs: {args}, {kwargs}"
+                print(log_message)
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(log_message + "\n")
+        return wrapper
+    return decorator
