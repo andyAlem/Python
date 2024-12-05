07e12fef835516b060667d2978688877204cc335
Andrej <andrej@mail.com>
2024-12-05 21:26:24 +0100

    Test for error if Input is str

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,8 @@
 from src.decorators import log
 
-def test_my_function(capsys):
+def test_decorator(capsys):
+    """Тестируем на успешное выполнение функции
+     и обработку исключений"""
     @log(filename="mylog.txt")
     def my_function(x, y):
         return x + y
@@ -9,10 +11,11 @@ def test_my_function(capsys):
     captured = capsys.readouterr()
     assert ("my_function is ok" in captured.out)
 
-def test_my_function_execution_correct(capsys):
-    @log(filename="mylog.txt")
-    def my_function(x, y):
-        return x + y
+    try:
+        my_function(2, "Test")
+    except TypeError:
+        captured = capsys.readouterr()
+        assert (
+                "my_function Error: unsupported operand type(s) for +: "
+                "'int' and 'str'. Inputs: (0, 'Test'), {}" in captured.out)
 
-    result = my_function(1, 2)
-    assert result == 3
