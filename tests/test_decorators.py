aaebd92f5ab30f18cc4a2faf796e42d7aade02bc
Andrej <andrej@mail.com>
2024-12-05 21:27:44 +0100

    performed flake8 and black

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,21 +1,23 @@
 from src.decorators import log
 
+
 def test_decorator(capsys):
     """Тестируем на успешное выполнение функции
-     и обработку исключений"""
+    и обработку исключений"""
+
     @log(filename="mylog.txt")
     def my_function(x, y):
         return x + y
 
-    my_function(1,2)
+    my_function(1, 2)
     captured = capsys.readouterr()
-    assert ("my_function is ok" in captured.out)
+    assert "my_function is ok" in captured.out
 
     try:
         my_function(2, "Test")
     except TypeError:
         captured = capsys.readouterr()
         assert (
-                "my_function Error: unsupported operand type(s) for +: "
-                "'int' and 'str'. Inputs: (0, 'Test'), {}" in captured.out)
-
+            "my_function Error: unsupported operand type(s) for +: "
+            "'int' and 'str'. Inputs: (0, 'Test'), {}" in captured.out
+        )
