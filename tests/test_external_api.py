603cf243d737eb5895acf84858b3fe626b2df734
Andrej <andrej@mail.com>
2024-12-11 12:55:26 +0100

    Added test with param for convert_to_rub

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,4 +1,4 @@
-
+import pytest
 from unittest.mock import patch
 
 from src.external_api import convert_to_rub
@@ -14,6 +14,23 @@ def test_convert_to_rub_api_failure(mock_get):
     assert result == 0
     mock_get.assert_called_once()
 
+
+
+@pytest.mark.parametrize("amount, currency, mock_result, expected", [
+    (1, "USD", {"result": 101}, 101),
+    (2, "EUR", {"result": 202}, 202),
+    (3, "USD", {"result": 303}, 303),
+])
+@patch("src.external_api.requests.get")
+def test_convert_to_rub_success(mock_get, amount, currency, mock_result, expected):
+    """Тестируем корректную работу функии"""
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = mock_result
+
+    result = convert_to_rub(amount, currency)
+
+    assert result == expected
+    mock_get.assert_called_once()
 
 
 
