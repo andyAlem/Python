52267e88ab6e3a4b743400536b1d8c66616f2509
Andrej <andrej@mail.com>
2024-12-11 14:27:29 +0100

    added fixture for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -153,3 +153,25 @@ def transactions_for_test():
             "to": "Счет 35383033474447895560"
         }
     ]
+
+@pytest.fixture
+def mock_data():
+    return '''
+    [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }
+    ]
+    '''
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,15 +4,41 @@ from src.utils import get_transactions
 from unittest.mock import patch, mock_open
 
 
-def test_get_transactions():
+def test_get_transactions(mock_data):
     """Тест на успешную загрузку операций из файла json"""
-    mock_data = '[{"id": "1109090", "amount": 0.998}, {"id": "122123", "amount": 9090}]'
     with patch("builtins.open", mock_open(read_data=mock_data)): #https://docs.python.org/3/library/unittest.mock.html
         result = get_transactions("hier_should_be_path_.json")
-    assert result == [{"id": "1109090", "amount": 0.998}, {"id": "122123", "amount": 9090}]
+    assert result == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }
+    ]
 
 
+def test_get_transactions_file_not_found():
+    """Файл отсутствует"""
+    with patch("builtins.open", side_effect=FileNotFoundError): #https://docs.python.org/3/library/unittest.mock.html#unittest.mock.patch
+        result = get_transactions("file_does_not_exist.json")
+    assert result == []
 
+
+def test_transactions_file_invalid_json():
+    """Тест на поврежденный файл, или который не может быть прочитан"""
+    with patch("builtins.open", mock_open(read_data="Currepted JSON")):
+        result = get_transactions("path_to_file.json")
+    assert result == []
 
 
 
