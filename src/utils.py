46571118daaaaaf3821fe6c86136b06ce6183394
Andrej <andrej@mail.com>
2024-12-11 12:26:06 +0100

    transaction_amount_in_rub fixed, used .get method for working with dict

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,42 @@
 import json
+from src.external_api import convert_to_rub
 
-def transactions_file(path):
+def get_transactions(path):
     try:
-        with open(path, "r", encoding="utf-8") as date_json:
-            return json.load(date_json)
+        with open(path, "r", encoding="utf-8") as data_json:
+            return json.load(data_json)
     except FileNotFoundError:
         return []
     except json.decoder.JSONDecodeError:
         return []
     except Exception as e:
-        print(f"Unexpected error: {e}")
+        print(f"Неизвестная ошибка {e}")
         return []
+
+
+def transaction_amount_in_rub(transactions, transaction_id): #https://sky.pro/media/ispolzovanie-dict-getkey-vmesto-dictkey-v-python/
+
+    for transaction in transactions:
+        if transaction.get("id") == transaction_id:
+            operation_amount = transaction.get("operationAmount", {})
+            amount = operation_amount.get("amount")
+            currency = operation_amount.get("currency", {}).get("code")
+
+            if not amount or not currency:
+                return "Ввод недействительный"
+
+            if currency == "RUB":
+                return float(amount)
+            else:
+                try:
+                    converted_amount = convert_to_rub(float(amount), currency)
+                    return round(converted_amount, 2)
+                except Exception as e:
+                    print(f"Ошибка конвертации: {e}")
+                    return "Конвертация невозможна"
+    return "Tранзакция не найдена"
+
+
+if __name__ == "__main__":
+    transactions = get_transactions("../data/operations.json")
+    print(transaction_amount_in_rub(transactions, 41428829))
