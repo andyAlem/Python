b0a91d69df08dd3915aefb0dfd560a88f281ad8f
Andrej <andrej@mail.com>
2024-12-13 09:50:39 +0100

    fixed convert_to_rub

--- src/external_api.py
+++ src/external_api.py
@@ -16,27 +16,32 @@ def convert_to_rub(transaction: dict):
     if not amount or not currency:
         raise ValueError("Неверные данные транзакции. Отсутствуют сумма или валюта.")
 
-    if currency not in {"USD", "EUR"}:
-        raise ValueError(f"Валюта {currency} не поддерживается.")
+    if currency == "RUB":
+        return float(amount)
 
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
-    headers = {"apikey": API_KEY}
+    if currency in {"USD", "EUR"}:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        headers = {"apikey": API_KEY}
 
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        json_result = response.json()
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            json_result = response.json()
 
-        if "result" in json_result:
-            return json_result["result"]
-        else:
+            if "result" in json_result:
+                return json_result["result"]
+            else:
+                return 0
+        except (RequestException, ValueError, KeyError):
             return 0
-    except (RequestException, ValueError, KeyError):
-        return 0
+    return 0
 
 
 # if __name__ == "__main__":
-# try:
-#     print(convert_to_rub(8221.37, "USD"))
-# except Exception as e:
-#     print(f"Error: {e}")
+#    try:
+#        transaction = {
+#            "operationAmount": {"amount": "8221.37", "currency": {"code": "RUB"}}
+#        }
+#        print(convert_to_rub(transaction))
+#    except Exception as e:
+#        print(f"Error: {e}")
