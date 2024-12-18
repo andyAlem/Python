d3018333ac857519e0235bfc3746d91d6d734848
Andrej <andrej@mail.com>
2024-12-18 07:14:20 +0100

    added logger for get_transactions_csv

--- src/transactions_csv.py
+++ src/transactions_csv.py
@@ -1,12 +1,26 @@
 import csv
 import os
+import logging
+
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("transactions_csv")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../logs", "transactions_csv.log"), mode="w"
+)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def get_transaction_csv(file_path):
 
     if not os.path.isfile(file_path):
+        logger.error(f"CSV файл не найден: {file_path}")
         raise FileNotFoundError(f"CSV файл '{file_path}' не найден.")
 
     if not file_path.endswith('.csv'):
+        logger.error("Неверный формат файла")
         raise ValueError("Неверный формат файла")
 
     try:
@@ -17,20 +31,24 @@ def get_transaction_csv(file_path):
                 transaction_list.append(row)
 
         if not transaction_list:
+            logger.warning(f"CSV файл пустой")
             raise ValueError("CSV файл пустой.")
 
+        logger.info(f"Успешно считано {len(transaction_list)} транзакций")
         return transaction_list
 
     except csv.Error as ex:
+        logger.error(f"Ошибка при чтении CSV-файла: {ex}")
         raise ValueError(f"Ошибка при чтении CSV-файла: {ex}")
     except Exception as ex:
+        logger.exception(f"Неизвестная ошибка при чтении CSV-файла")
         raise ValueError(f"Ошибка при чтении CSV-файла: {ex}")
 
 
-# if __name__ == "__main__":
+#if __name__ == "__main__":
 #     csv_file = "../data/transactions.csv"
 #
-#     try:
+#    try:
 #         transactions = get_transaction_csv(csv_file)
 #         print("Транзакции из CSV файла:")
 #         for transaction in transactions:
